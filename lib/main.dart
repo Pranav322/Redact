a814d057927faf33799640af9ac2079931b21b4f
RAUSHAN KUMAR THAKUR <rnkp755@gmail.com>
2024-09-18 22:31:12 +0530

    Home Page Added

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/services.dart';
 import 'auth/login_page.dart';
 import 'main_screen.dart';
 import 'package:redact/themes/theme.dart';
+import 'welcome_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]);
   await Firebase.initializeApp();
   await FilePicker.platform.clearTemporaryFiles();
   runApp(MyApp());
@@ -23,14 +20,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Redact',
-      theme: AppTheme.darkTheme, // Use the theme from app_theme.dart
+      theme: AppTheme.darkTheme,
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.active) {
             User? user = snapshot.data;
             if (user == null) {
-              return LoginPage();
+              return WelcomeScreen();
             }
             return MainScreen();
           }
@@ -44,3 +41,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+//main.dart
